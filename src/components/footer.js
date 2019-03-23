d5d1b9e32d5773a01d34f5f379c1bd4b25a8783a
frdvorak <frantisek.dvorak011@gmail.com>
2019-03-23 14:49:49 +0000

    mobile styles complete

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,14 @@ class Footer extends Component {
     state = {}
     render() {
         return (
-            <footer>Footer</footer>
+            <footer className="footer">
+                <h1 className="footer__h1">Subscribe to our newsletter</h1>
+                <form className="footer__form" action="">
+                    <label className="footer__form__label" for="subscribe">Hi! Want to know when a new job is posted?</label>
+                    <input className="footer__form__input" type="text" id="subscribe" name="email" placeholder="Enter your email address" />
+                    <imput className="footer__form__submit" type="submit" id="submit" name="submit" >Subscribe</imput>
+                </form>
+            </footer>
         );
     }
 }
