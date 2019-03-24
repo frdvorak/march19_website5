8e6b0a356d7f62e1d732d7cb0c6d83c9d4bc083c
frdvorak <frantisek.dvorak011@gmail.com>
2019-03-24 19:20:20 +0000

    styles

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+
 
 class Footer extends Component {
     // Input field 'placeholder' text animation (don't allow placeholder text to slide back unless the field is empty)
@@ -16,36 +16,10 @@ class Footer extends Component {
         const alert = document.querySelector('.footer__alert');
         alert.innerHTML = message;
         alert.classList.remove('hidden');
-        setTimeout(() => {
-            alert.classList.add('hidden');
-        }, 5000)
     }
     // Validate email / Submit form
     handleSubmit = async (e) => {
-        e.preventDefault();
-        const emailInput = document.getElementById('subscribe');
-        const submitButton = document.getElementById('submit');
-        const email = emailInput.value;
-        const regx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        let message = '';
-        if (email === '') {
-            message = 'Please enter your email';
-            this.showAlert(message);
-        } else if (!regx.test(email)) {
-            message = 'Please enter valid email in format: name@domain.com';
-            this.showAlert(message);
-        } else if (regx.test(email)) {
-            console.log('Form Submitted', email);
-            // post subscription request
-            //axios.post('http://localhost:4000/recipes/add', email)
-            //    .then(res => console.log(res.data));
-            //clear input field
-            emailInput.value = '';
-            emailInput.classList.remove('focused');
-            message = 'Subscribed ' + email;
-            this.showAlert(message);
-            submitButton.setAttribute.name = 'Thank you';
-        }
+
     }
     render() {
         return (
@@ -58,6 +32,7 @@ class Footer extends Component {
                     <label className="footer__form__label" htmlFor="subscribe">Enter your email address</label>
                     <input className="footer__form__submit" type="submit" id="submit" name="Submit" />
                 </form>
+                <p className="footer__success hidden">Thank you for subscribing to our newsletter</p>
             </footer>
         );
     }
