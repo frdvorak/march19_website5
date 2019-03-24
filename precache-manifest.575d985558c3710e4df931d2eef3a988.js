6f16eda0cf07dc0968a6af0667c12e742177abde
frdvorak <frantisek.dvorak011@gmail.com>
2019-03-24 17:08:50 +0000

    Updates

---
+++ precache-manifest.575d985558c3710e4df931d2eef3a988.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5517f0bc296b99bf9519",
+    "url": "/static/js/main.d1b2e8ad.chunk.js"
+  },
+  {
+    "revision": "6d314bbf69605de3710d",
+    "url": "/static/js/2.894eb548.chunk.js"
+  },
+  {
+    "revision": "5517f0bc296b99bf9519",
+    "url": "/static/css/main.d633bc83.chunk.css"
+  },
+  {
+    "revision": "152ca08cf8649c0b35afc2ae1d47cee0",
+    "url": "/index.html"
+  }
+];
